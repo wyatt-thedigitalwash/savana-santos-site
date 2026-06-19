01a30d3a268d7fc9671346ce01220aa23ae0ff2a
WyattBensman <wyatt@thedigitalwash.com>
2026-06-18 22:02:23 -0400

    Fix sitemap type error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,11 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://DOMAIN.com';
   return [
-    { url: baseUrl, changeFrequency: 'weekly', priority: 1.0 },
-  ].map(page => ({ ...page, lastModified: new Date() }));
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 1.0,
+    },
+  ];
 }
